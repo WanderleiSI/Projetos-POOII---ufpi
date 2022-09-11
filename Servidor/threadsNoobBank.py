1bd2ccc369791c258548b81f50c436fd7d79774d
WanderleiSI <wanderleirochavasco@hotmail.com>
2022-09-11 16:01:14 -0300

    Problemas em encerrar os clientes

--- Servidor/threadsNoobBank.py
+++ Servidor/threadsNoobBank.py
@@ -25,7 +25,7 @@ class Threads(threading.Thread):
             
             self.sinc.acquire()
             if requisicao[0] == 'CLIENTE':
-                self.servidor.requisicaoChecagem('CLIENTE',(requisicao[1],requisicao[2]))
+                self.servidor.requisicaoChecagem('CLIENTE',(requisicao[1],requisicao[2]),self.clientSock)
             elif requisicao == 'CONECTAR_BANCO_DE_DADOS':
                 self.servidor.bd.conectarBanco()
                 if self.servidor.bd.conexao.is_connected():
@@ -33,16 +33,17 @@ class Threads(threading.Thread):
                 else:
                     self.servidor.con.send("DESCONECTADO".encode())
             elif requisicao[0] == 'CADASTRO':
-                self.servidor.requisicaoChecagem('CADASTRO',(requisicao[1],requisicao[2],requisicao[3],requisicao[4],requisicao[5]))
+                self.servidor.requisicaoChecagem('CADASTRO',(requisicao[1],requisicao[2],requisicao[3],requisicao[4],requisicao[5]),self.clientSock)
             elif requisicao[0] == 'DEPOSITO':
-                self.servidor.requisicaoValor('DEPOSITO',(requisicao[1],requisicao[2],requisicao[3],requisicao[4]))
+                self.servidor.requisicaoValor('DEPOSITO',(requisicao[1],requisicao[2],requisicao[3],requisicao[4]),self.clientSock)
             elif requisicao[0] == 'SAQUE':
-                self.servidor.requisicaoValor('SAQUE',(requisicao[1],requisicao[2],requisicao[3],requisicao[4]))
+                self.servidor.requisicaoValor('SAQUE',(requisicao[1],requisicao[2],requisicao[3],requisicao[4]),self.clientSock)
             elif requisicao[0] == 'TRANSFERENCIA':
-                self.servidor.requisicaoValor('TRANSFERENCIA',(requisicao[1],requisicao[2],requisicao[3],requisicao[4],requisicao[5]))
+                self.servidor.requisicaoValor('TRANSFERENCIA',(requisicao[1],requisicao[2],requisicao[3],requisicao[4],requisicao[5]),self.clientSock)
             elif requisicao[0] == 'DESCONECTAR_SERVIDOR':
                 self.servidor.desconectarServidor()
-                #break
+                self.sinc.release()
+                break
             elif requisicao[0] == 'HISTORICO':
                 print(requisicao)
                 historico = self.servidor.bd.PreencheHistorico(requisicao[1])
