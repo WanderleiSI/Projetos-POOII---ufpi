57913d96100e1961f40d39eb1bb661b60c2d0336
WanderleiSI <wanderleirochavasco@hotmail.com>
2022-09-11 20:23:09 -0300

    Historico OK Corrigir reconexao

--- Servidor/threadsNoobBank.py
+++ Servidor/threadsNoobBank.py
@@ -10,7 +10,7 @@ class Threads(threading.Thread):
         #self.servidor = copy.deepcopy(servidor)
         #self.servidor.con = self.clientSock
         self.servidor = servidor
-        servidor.con = self.clientSock
+        #servidor.con = self.clientSock
     def run(self):
         #requisicao = servidor.con.recv(1024)
         #requisicao = requisicao.decode()
@@ -41,18 +41,22 @@ class Threads(threading.Thread):
             elif requisicao[0] == 'TRANSFERENCIA':
                 self.servidor.requisicaoValor('TRANSFERENCIA',(requisicao[1],requisicao[2],requisicao[3],requisicao[4],requisicao[5]),self.clientSock)
             elif requisicao[0] == 'DESCONECTAR_SERVIDOR':
-                self.servidor.desconectarServidor()
-                self.sinc.release()
+                #self.servidor.desconectarServidor()
+                #self.sinc.release()
+                self.clientSock.close()
                 break
             elif requisicao[0] == 'HISTORICO':
                 print(requisicao)
                 historico = self.servidor.bd.PreencheHistorico(requisicao[1])
+                self.servidor.conexao = self.clientSock
                 if len(historico) == 0:
-                    self.servidor.con.send('False'.encode())
+                    #self.servidor.con.send('False'.encode())
+                    self.servidor.conexao.send('False'.encode())
                 else:
                     retorno = ""
                     for h in historico:
                         retorno += F"{h}"
                         retorno += "|"
-                    self.servidor.con.send(retorno.encode())
+                    #self.servidor.con.send(retorno.encode())
+                    self.servidor.conexao.send(retorno.encode())
             self.sinc.release()            
